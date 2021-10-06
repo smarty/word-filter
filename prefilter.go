da994a56bda7841287d88deb852361e522eab508
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2021-10-06 08:00:41 -0600

    Using tree method; combined files. Formalized interface.

--- prefilter.go
+++ prefilter.go
@@ -1,82 +1,98 @@
 package prefilter
 
-import "strings"
-
-func main() {
-}
-
 type Prefilter interface {
-	IsAllowed(string) bool
+	IsAllowed(value string) bool
 }
 
-type prefilter struct {
-	buffer     []byte
-	restricted []string
-	Prefilter
+type treeNode struct {
+	wordFragmentUpper byte
+	wordFragmentLower byte
+	isWord            bool
+	children          []*treeNode
 }
 
-func newPrefilter(bufferSize int, reserved ...string) Prefilter {
-	b := make([]byte, bufferSize, bufferSize)
-	m := make(map[string]struct{})
-	for _, r := range reserved {
-		m[r] = struct{}{}
+func New(reserved ...string) Prefilter {
+	this := &treeNode{}
+
+	for _, item := range reserved {
+		this.add(item)
 	}
-	restricted := make([]string, 0, len(m))
-	for key, _ := range m {
-		restricted = append(restricted, key)
-	}
-	return &prefilter{
-		buffer:     b,
-		restricted: restricted,
-	}
+
+	return this
 }
 
-func (this *prefilter) IsAllowed(input string) bool {
-	copy(this.buffer[:], input)
-	start := 0
-	for end, r := range this.buffer {
-		//check if lowercase letter
-		if !('a' <= r && r <= 'z') {
-			//check if uppercase letter, change to lower
-			if 'A' <= r && r <= 'Z' {
-				this.buffer[end] += 'a' - 'A'
-			}
-		}
+func (this *treeNode) add(value string) {
+	if len(value) == 0 {
+		this.isWord = true
+		return
+	}
 
-		if r == ' ' || r == '\n' || r == '\t' {
-			word := this.buffer[start:end]
-			if contains(this.restricted, word) {
-				return false
-			}
-			start = end + 1
+	wordFragment := value[0]
+
+	// check if lowercase letter
+	if !('a' <= wordFragment && wordFragment <= 'z') {
+		// check if uppercase letter, change to lower
+		if 'A' <= wordFragment && wordFragment <= 'Z' {
+			wordFragment += 'a' - 'A'
 		}
 	}
 
-	// restricted word may be at eof
-	word := this.buffer[start:]
-	if contains(this.restricted, word) {
-		return false
-	}
-
-	return true
-}
-
-func contains(restricted []string, word []byte) bool {
-	for _, s := range restricted {
-		if s == string(word) {
-			return true
+	remainingWord := value[1:]
+	for _, child := range this.children {
+		if child.wordFragmentLower == wordFragment {
+			child.add(remainingWord)
+			return
 		}
 	}
 
-	return false
+	child := &treeNode{
+		wordFragmentLower: wordFragment,
+		wordFragmentUpper: wordFragment - ('a' - 'A'),
+		children:          nil,
+		isWord:            false,
+	}
+
+	this.children = append(this.children, child)
+	child.add(remainingWord)
 }
 
-func (this *prefilter) IsAllowed2(input string) bool {
-	input = strings.ToUpper(input)
-	for _, word := range this.restricted {
-		if strings.Contains(input, word) {
+func (this *treeNode) IsAllowed(input string) bool {
+	allowed := true
+	raw := []byte(input)
+	inputLength := len(input)
+
+	for index := 0; index < inputLength; index++ {
+		if allowed, index = this.isAllowed(raw, index); !allowed {
 			return false
 		}
 	}
+
 	return true
+}
+func (this *treeNode) isAllowed(input []byte, index int) (bool, int) {
+	if len(input) == index || input[index] == ' ' || input[index] == '\n' || input[index] == '\t' {
+		if this.isWord == true {
+			return false, index
+		}
+
+		return true, index
+	}
+
+	for _, child := range this.children {
+		if input[index] == child.wordFragmentLower {
+			return child.isAllowed(input, index+1)
+
+		} else if input[index] == child.wordFragmentUpper {
+			return child.isAllowed(input, index+1)
+
+		} else {
+			for input[index] != ' ' && input[index] != '\n' && input[index] != '\t' && len(input) != index+1 {
+				index += 1
+			}
+
+			return true, index
+		}
+	}
+
+	return true, index
 }
--- prefilter_test.go
+++ prefilter_test.go
@@ -1,57 +1,45 @@
 package prefilter
 
-import (
-	"testing"
-)
+import "testing"
 
-func TestPrefiltering(t *testing.T) {
-	assertPrefilter(t, "WORD", false, "word")
-	assertPrefilter(t, "word", false, "word")
-	assertPrefilter(t, "Word", false, "word")
-	assertPrefilter(t, "woRd", false, "word")
+func TestTreePrefiltering(t *testing.T) {
+	assertTreePrefilter(t, "WORD", false, "word")
+	assertTreePrefilter(t, "word", false, "word")
+	assertTreePrefilter(t, "woRd", false, "word")
 
-	assertPrefilter(t, "word", false, "word", "restricted")
-	assertPrefilter(t, "restricted", false, "word", "restricted")
-	assertPrefilter(t, "A sentence with a word in the middle", false, "word", "restricted")
-	assertPrefilter(t, "A sentence with a word\n in the middle", false, "word", "restricted")
-	assertPrefilter(t, "A sentence with a word	 in the middle", false, "word", "restricted")
+	assertTreePrefilter(t, "A sentence with a word in the middle", false, "word", "restricted")
+	assertTreePrefilter(t, "A sentence with a word\n in the middle", false, "word", "restricted")
+	assertTreePrefilter(t, "A sentence with a word	 in the middle", false, "word", "restricted")
 
-	assertPrefilter(t, "Only good words", true, "word", "restricted")
-	assertPrefilter(t, "A good sentence 123 with numbers", true, "word", "restricted")
-	assertPrefilter(t, "Trailing space ", true, "word", "restricted")
+	assertTreePrefilter(t, "Only good words", true, "word", "restricted")
+	assertTreePrefilter(t, "A good sentence 123 with numbers", true, "word", "restricted")
+	assertTreePrefilter(t, "Trailing space ", true, "word", "restricted")
 
-	assertPrefilter(t, "A bad sentence with numbers word1", false, "word1", "restricted")
+	assertTreePrefilter(t, "A bad sentence with numbers word1", false, "word1", "restricted")
 }
 
-func assertPrefilter(t *testing.T, input string, expected bool, reserved ...string) {
+func assertTreePrefilter(t *testing.T, input string, expected bool, reserved ...string) {
 	t.Helper()
 
-	reservedWords := newPrefilter(len(input), reserved...)
-	actual := reservedWords.IsAllowed(input)
+	reservedTree := New(reserved...)
+	actual := reservedTree.IsAllowed(input)
 
-	if actual == expected {
-		return
+	if actual != expected {
+		t.Errorf("\nExpected: %t\nActual:   %t\n", expected, actual)
 	}
-
-	t.Errorf("\n"+
-		"Expected: %t\n"+
-		"Actual:   %t\n",
-		expected,
-		actual,
-	)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-func BenchmarkTest(b *testing.B) {
+func BenchmarkTreeTest(b *testing.B) {
 	b.ReportAllocs()
 	// 256 chars
 	stringToCheck := "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut " +
 		"labore et dolore magna aliqua. Ut EMIM ad minim veniam, quis nostrud exercitation ULLAMCO laboris nisi ut " +
 		"aliquip ex ea commodo consequat. Duis aute irure d RANDOM"
-	reservedWords := newPrefilter(len(stringToCheck), "ANOTHER", "RANDOM", "WORD")
+	reservedTree := New("ANOTHER", "RANDOM", "WORD")
 	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
-		_ = reservedWords.IsAllowed(stringToCheck)
+		_ = reservedTree.IsAllowed(stringToCheck)
 	}
 }
